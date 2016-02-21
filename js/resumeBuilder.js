5d8189c0504c25f47818df80eae2652468c8f6d4
Jorge Abreu <jnabreu@gmail.com>
2016-02-21 15:06:38 +0000

    Added Cenas1
    asdsdasd asd

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,25 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+HTMLheaderNameReplaced = HTMLheaderName.replace("%data%", "Jorge Abreu");
+HTMLheaderRoleReplaced = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#header").append(HTMLheaderNameReplaced);
+$("#header").append(HTMLheaderRoleReplaced);
+
+var bio = {
+	name: "Jorge Abreu",
+	role: "Web Developer",
+	contactInfo: {
+		cellPhone: 888999444,
+		address: "Rua das Cenas",
+	},
+	pict: "",
+	welcomeMsg:"Ola cenas cenas adeus",
+	skills: ["code", "speak", "walk", "eat"]
+};
+
+$("#header").append(bio.name);
+$("#header").append(bio.role);
+$("#header").append(bio.contactInfo.cellPhone);
+$("#header").append(bio.contactInfo.address);
+
+bio["car"] = "Ford";
+$("#header").append(bio.car);
